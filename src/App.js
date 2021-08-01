f4f91bed383a65b4ebc29c2e0ffdcb4f33dae8e1
Christos Deretzis <deretzischristos@gmail.com>
2021-08-01 18:49:24 +0300

    Add Search Bar Container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
+import React, {Component} from 'react'
 import './App.css';
 import Card from './components/Card/Card';
 import Loader from './components/Loader/Loader';
 import Header from './containers/Header/Header';
+import SearchBar from './containers/SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="AppWrapper">
-      <Header />
-      <main className="AppMain">
-        <Card>
-          <Loader /> 
-        </Card>
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    searchBarInput: "",
+    weatherDetails: {
+      temperature: null,
+      description: ''
+    },
+    loading: false,
+    error: false
+  }
+
+  searchBarHandler = (e) => {
+    this.setState({
+      searchBarInput: e.target.value
+    });
+  }
+
+
+  setWeather = () => {
+    console.log("Hello")
+  }
+
+  render() {
+    return (
+      <div className="AppWrapper">
+        <Header />
+        <main className="AppMain">
+        <SearchBar
+          value={this.state.searchBarInput}
+          onChangeHandler={this.searchBarHandler}
+          onClickHandler={this.setWeather}
+          error={this.state.error} />
+          <Card>
+            <Loader /> 
+          </Card>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
