2dc3a7a6cf25579050b121de5e8eb75c45f010ba
Christos Deretzis <deretzischristos@gmail.com>
2021-08-01 08:57:36 +0300

    Initial Commit

---
+++ src/containers/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = (props) => {
+    return (
+        <header className="Header">
+            <h1 className="Logo">
+                My Header
+            </h1>
+        </header>
+    );
+}
+
+export default Header;
